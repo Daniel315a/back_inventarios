2cc660b2ef30af70924dd2212b095ec4938688d5
Daniel López <daniel315.a@gmail.com>
2021-07-14 15:37:46 -0500

    Se crean el modelo y el controlador de detalles devolución

---
+++ Core/Models/DetalleDevolucion.php
@@ -0,0 +1,19 @@
+<?php namespace Models;
+
+    class DetalleRemision extends BaseModelo
+    {
+
+        public $id;
+        public $producto;
+        public $cantidad;
+        public $precio;
+        public $inventario_interno;
+        public $descripcion;
+
+        /**
+         * Métodos
+         */
+
+    }
+    
+?>
