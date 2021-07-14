3a8f60df220352e8ed29fc30e3aaccbaac474b40
Daniel López <daniel315.a@gmail.com>
2021-07-14 12:24:51 -0500

    Se crea la funcionalidad de crear remisión

--- Core/Controllers/Remision.php
+++ Core/Controllers/Remision.php
@@ -3,6 +3,32 @@
     class Remision
     {
         
+        public static function post()
+        {
+            $respuesta = \Respuesta::obtenerDefault();
+            $remision = new \Models\Remision();
+            $parametrosOk = true;
+
+            if(isset($_POST['solicitud']))
+            {
+                $solicitud = $_POST['solicitud'];
+
+                if($solicitud == 'crear')
+                {
+                    $remision->factura = new \Models\Factura($_POST['id_factura']);
+                    $remision->encargado = new \Models\Persona($_POST['id_encargado']);
+                    $remision->estado = false;
+                    $remision->notas = $_POST['notas'];
+                    $remision->fecha_entrega = $_POST['fecha_entrega'];
+                    $remision->fecha_instalacion = $_POST['fecha_instalacion'];
+
+                    $respuesta = $remision->crear();
+                }
+            }
+
+            \responder($respuesta, $parametrosOk);
+        }
+
     }
     
 ?>
--- Core/Models/Remision.php
+++ Core/Models/Remision.php
@@ -1,8 +1,48 @@
 <?php namespace Models;
 
-    class Remision extends BaseModel
+    class Remision extends BaseModelo
     {
-        
+    
+        public $id;
+        public $factura;
+        public $encargado;
+        public $estado;
+        public $nombre_archivo_soporte;
+        public $notas;
+        public $fecha_entrega;
+        public $fecha_instalacion;
+
+        /**
+         * Métodos
+         */
+
+        public function crear()
+        {
+            $sql = "
+            INSERT INTO decora_transforma.remisiones
+            (
+                factura,
+                encargado,
+                estado,
+                notas,
+                fecha_entrega,
+                fecha_instalacion
+            )
+            VALUES
+            (
+                {$this->factura->id},
+                {$this->encargado->id},
+                0,
+                '{$this->notas}',
+                '{$this->fecha_entrega}',
+                '{$this->fecha_instalacion}'
+            );";
+
+            $this->conexion->execCommand($sql);
+
+            return $this->obtenerRespuesta($this, true, false);
+        }
+
     }
     
 ?>
